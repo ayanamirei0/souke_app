f08c35ccbeab6b9d69f2bf86707754cfa6e764f6
LiHongLiang <L200649@live.com>
2019-01-10 13:59:57 +0800

    我的课程部分课程标题加粗bug

--- src/web/AccountManagement/MyCourse.vue
+++ src/web/AccountManagement/MyCourse.vue
@@ -13,7 +13,7 @@
       <div class="clearfix">
         <div class="searchLessons-content-title fl">
             <div class="searchLessons-content-bnt-xk marginright20 margintop5">{{time.type}}</div>
-            <router-link :to='"SearchLessonsDetails/"+time.id' v-if="time.state_text=='未结课' || time.state_text=='未开班'">{{time.title}}</router-link>
+            <router-link :to='"SearchLessonsDetails/"+time.id' v-if="time.state_text=='未结课' || time.state_text=='未开班'"><b>{{time.title}}</b></router-link>
             <em v-else-if="time.state_text=='已停班' || time.state_text=='已结课'"><b>{{time.title}}</b></em>
         </div>
       </div>
